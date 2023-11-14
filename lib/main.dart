8200c27c319f5162927fec3cf8be74bc30bc87ff
Dayrons <dayronstovar@gmail.com>
2023-11-14 16:07:16 -0400

    modificacion de envio de archivo ftp

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:ftpconnect/ftpconnect.dart';
-
+import 'package:path_provider/path_provider.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -25,13 +27,58 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+
+  Future<String> get _localPath async {
+  final directory = await getApplicationDocumentsDirectory();
+  
+  return directory.path;
+}
+
+
+
   const MyHomePage({super.key});
+
+
+  List<Map>_getPasswords(){
+    return [
+    {
+      "titulo":"Provincial",
+      "password":"123456",
+    },
+    {
+      "titulo":"Venezuela",
+      "password":"123456",
+    },
+    {
+      "titulo":"Mercantil",
+      "password":"123456",
+    },
+    {
+      "titulo":"Binance",
+      "password":"123456",
+    },
+    {
+      "titulo":"Banca Amiga",
+      "password":"123456",
+    },
+    {
+      "titulo":"Paypal",
+      "password":"123456",
+    },
+    {
+      "titulo":"Provincial",
+      "password":"123456",
+    },
+  ];
+  }
 
   void _connect()async{
     FTPConnect ftpConnect = FTPConnect('10.10.10.85', port: 2121,user:'user', pass:'password');
-    File fileToUpload = File('file.txt');
+    final path = await _localPath;
+    File file = File('$path/password.txt');
+    file.writeAsString( json.encode(_getPasswords()));
     await ftpConnect.connect();
-    bool res = await ftpConnect.uploadFileWithRetry(fileToUpload, pRetryCount: 2);
+    bool res = await ftpConnect.uploadFileWithRetry(file, pRetryCount: 2);
     await ftpConnect.disconnect();
     print(res);
   }
